6dfb17a0f7a73b6cad605bd457c45db1033f3e66
Roman Kim <kimcrent@gmail.com>
2024-07-07 18:56:28 +0300

    end lesson 31

--- src/lessons/Lesson_31.java
+++ src/lessons/Lesson_31.java
@@ -1,4 +1,20 @@
 package lessons;
 
 public class Lesson_31 {
+    public static void main(String[] args) {
+        int x = 32;
+        short s = 16;
+        byte b = 126;
+        long l = 123L;
+        double d = 12.3;
+        float f = 123.2F;
+        long s1 = 1231L;
+
+        long h = x;
+
+        int a = (int)l;
+        System.out.println(a);
+        long p = Math.round(d);
+        System.out.println(p);
+    }
 }
