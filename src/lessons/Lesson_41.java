760b727d16ed5925600aa230ec752e2e9d928a0d
Roman Kim <kimcrent@gmail.com>
2024-07-07 22:31:10 +0300

    end lesson 41

---
+++ src/lessons/Lesson_41.java
@@ -0,0 +1,23 @@
+package lessons;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Lesson_41 {
+    public static void main(String[] args) {
+        try {
+            run();
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e){
+
+        }
+//        catch (ParseException e){
+//
+//        }
+    }
+    public static void run() throws IOException, ParseException, IllegalArgumentException {
+
+    }
+}
