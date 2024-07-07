743c6fccfe5995d667a74d8a0cd32629ed721887
Roman Kim <kimcrent@gmail.com>
2024-07-07 19:09:14 +0300

    end lesson 33

--- src/lessons/Lesson_33.java
+++ src/lessons/Lesson_33.java
@@ -1,7 +1,23 @@
 package lessons;
 
+import lessons.lesson30.Animal;
+import lessons.lesson30.Dog;
+
 public class Lesson_33 {
     public static void main(String[] args) {
+        //Upcasting - восходящее преобразование
+        Dog dog = new Dog();
 
+        Animal animal = new Dog();
+        animal.eat();
+
+        //Downcasting - Низходящее преобразование
+
+        Dog dog1 = (Dog) animal;
+        dog1.bark();
+
+//        Animal animal1 = new Animal();
+//        Dog dog2 = (Dog) animal1;
+//        dog2.bark();
     }
 }
