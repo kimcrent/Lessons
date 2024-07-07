5b10e7cac12f6daa00ec2e1d1b8fe4241cd87097
Roman Kim <kimcrent@gmail.com>
2024-07-07 17:25:49 +0300

    add access level img

--- src/lessons/Lesson_29.java
+++ src/lessons/Lesson_29.java
@@ -1,8 +1,13 @@
 package lessons;
 
+import lessons.packeg1.Person;
+
 public class Lesson_29 {
     public int id;
+    private int x;
     public static void main(String[] args) {
-        //public, private, default, protected
+        //public, private, default(Всё что дефолт доступно в пределах этого объекта), protected (достпуны в пределах
+        // одного паекта, доступны всем подклассам, даду ести эти подклассы вне пакета)
+      Test test = new Test();
     }
 }
