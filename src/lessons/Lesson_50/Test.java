7ed4d346077fa3f2119c95dd210e3a770ff31350
Roman Kim <kimcrent@gmail.com>
2024-07-10 21:21:56 +0300

    end alishev curse

---
+++ src/lessons/Lesson_50/Test.java
@@ -0,0 +1,13 @@
+package lessons.Lesson_50;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(fac(4));
+    }
+    private static int fac(int n){
+        if (n == 1){
+            return 1;
+        }
+        return n * fac(n - 1);
+    }
+}
